3615fffa14dcdcb4bca688877303a1d6fb603d64
Sheventon <tores.fernando.real@gmail.com>
2020-11-06 22:21:15 +0300

    final front

--- src/main/webapp/js/open_hide_window.js
+++ src/main/webapp/js/open_hide_window.js
@@ -1,6 +1,6 @@
 function toogle_visibility(id) {
     var e = document.getElementById(id);
-    var button = document.getElementById('timetable-button')
+    var button = document.getElementById('timetable-button');
     if(e.style.display === 'block') {
         e.style.display = 'none';
         button.value = "Показать";
@@ -9,4 +9,14 @@ function toogle_visibility(id) {
         e.style.display = 'block';
         button.value = "Скрыть";
     }
+}
+
+function update_visibility(id) {
+    var e = document.getElementById(id);
+        e.style.display = 'flex';
+}
+
+function hide_update_visibility(id) {
+    var e = document.getElementById(id);
+        e.style.display = 'none';
 }
